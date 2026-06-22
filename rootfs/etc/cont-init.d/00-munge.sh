3106eea925135f7e61a0293bfb0bf3911eb92a40
Zackarie Vinckier <32618947+zckv@users.noreply.github.com>
2026-06-22 17:22:14 +0200

    add run argument for munge key (#6)
    * add MUNGE_KEY_BASE64 env arg
    
    * remove mkdir step in Dockerfile
    
    * add small doc

--- rootfs/etc/cont-init.d/00-munge.sh
+++ rootfs/etc/cont-init.d/00-munge.sh
@@ -4,7 +4,12 @@ set -eu
 install -d -m 700 /var/log/munge /run/munge
 
 if [ ! -f /etc/munge/munge.key ]; then
-    create-munge-key
+    if [ -n "${MUNGE_KEY_BASE64:-}" ]; then
+        echo "$MUNGE_KEY_BASE64" | base64 -d > /etc/munge/munge.key
+        chmod 400 /etc/munge/munge.key
+    else
+        create-munge-key
+    fi
 fi
 
 chown -R root:root /var/log/munge /var/lib/munge /run/munge /etc/munge
